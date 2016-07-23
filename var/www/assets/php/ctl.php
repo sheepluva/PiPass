1ab4f30d077061ac1adbd68e4d884e8d334f8aae
sheepluva <sheepluva@users.noreply.github.com>
2016-07-24 01:51:04 +0200

    code tweaks

--- var/www/assets/php/ctl.php
+++ var/www/assets/php/ctl.php
@@ -30,8 +30,7 @@ if (!in_array($command, $whitelist)) {
 $shcmdarg = escapeshellarg($command);
 
 if ($command === 'dashboard') {
-    $cfg = json_decode(file_get_contents('../../config/pipass_config.json'),true);
-    $shdasharg = escapeshellarg($cfg['DASHBOARD']);
+    $shdasharg = escapeshellarg($getpost['DASHBOARD']);
     exec("sudo \"$bin\" $shcmdarg $shdasharg > /dev/null");
 }
 else exec("sudo \"$bin\" $shcmdarg > /dev/null");
